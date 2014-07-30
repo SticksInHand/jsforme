062aaadb1989bbe8e3bf2330aeef39cf97ebcb7c
thehappyman <1246505492@qq.com>
2014-07-30 16:38:41 +0800

    img change

---
+++ js/2-10.js
@@ -0,0 +1,14 @@
+/**
+ * Created by M.Jin on 14-7-30.
+ */
+window.onload = function(){
+    var oImg = document.getElementById("box").getElementsByTagName("img");
+    var oDiv = document.getElementsByTagName("div")[0];
+
+    for(var i = 0;i < oImg.length;i++){
+        oImg[i].onmouseover = function(){
+            var img = new Image();
+            img.src = oImg[0].src = this.src.replace(/small/,"big");
+        }
+    }
+};
