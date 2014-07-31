7cfae9a5793d2c4b83226d64aa97fba5929e000b
thehappyman <1246505492@qq.com>
2014-07-31 14:58:44 +0800

    my note

--- js/4-1.js
+++ js/4-1.js
@@ -10,6 +10,75 @@ var get = {
             return oparent.getElementsByClassName(sclass);
         }else{
             var results = new Array();
+            var allNode = oparent.getElementsByTagName("*");
+            for(var p in allNode){
+                if(allNode[p] == sclass){
+                    results.push(allNode[p]);
+                }
+            }
+
+            return results;
+        }
+    },
+    byTagName:function(elem,obj){
+        return (obj||document).getElementsByTagName(elem);
+    }
+};
+
+window.onload = function(){
+    var oNav = get.byId("nav");
+    var oLi = get.byTagName("li",document);
+    var aSubNav = get.byClass("subnav",oNav);
+    var oSubNav = oArrow = null;
+
+    for(var i = 0;i<oLi.length;i++){
+        oLi[i].onmouseover = function(){
+
+            for(var p = 0;p<aSubNav.length;p++){
+                aSubNav[p].style.display = "none";
+            }
+
+            oSubNav = get.byClass("subnav",this);
+            oArrow = get.byClass("arrow",this);
+            if(oSubNav[0]){
+                oSubNav[0].style.display = "block";
+            }
+            oNav.offsetWidth - this.offsetLeft > oSubNav[0].offsetWidth ? oSubNav[0].style.left = this.offsetLeft + "px" : oSubNav[0].style.right = 0;
+
+
         }
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
